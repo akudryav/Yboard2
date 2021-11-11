66d723751c4324ee4f143d0067b236acbac76e16
Anton <antonsk@mail.ru>
2021-11-11 23:21:04 +0300

    Поправка карточки объявления

--- views/adverts/_item.php
+++ views/adverts/_item.php
@@ -10,15 +10,19 @@ use yii\helpers\StringHelper;
 $image = $model->getImage();
 ?>
 
-<div class="card border-secondary mb-3">
-    <?php echo Html::img($image->getUrl('350x'), ['class' => 'card-img-top', 'alt' => $model->name]); ?>
-    <div class="card-body">
-        <h5 class="card-title"><?php echo Html::encode($model->name); ?></h5>
-        <p class="card-text"><?php echo StringHelper::truncate($model->text, 150); ?></p>
-        <?php echo Html::a(Yii::t('adv', 'Details'),
-            ['adverts/view', 'id' => $model->id], ['class' => 'btn btn-primary']); ?>
-        <?php echo Html::a(Yii::t('adv', 'Message'),
-            '#', ['class' => 'card-link']); ?>
+<div class="card border-secondary mb-3" style="width: 15rem;">
+    <a href="<?= Url::to(['adverts/view', 'id' => $model->id]) ?>" class="text-decoration-none">
+        <?php echo Html::img($image->getUrl('x220'), ['class' => 'card-img-top', 'alt' => $model->name]); ?>
+        <div class="card-body">
+            <p class="card-subtitle mb-2 text-muted"><?= $model->price ?><span>₽</span></p>
+            <h5 class="card-title text-dark"><?php echo Html::encode($model->name); ?></h5>
+        </div>
+    </a>
+    <div class="card-footer text-muted">
+        <i class="fa fa-eye"></i><?= $model->views ?>
+        <a href="javascript:void(0)" title="В избранное"
+           onclick="setFavoriteAdv(<?= $model->id ?>, this)">
+            <i class="fa fa-bookmark-o"></i></a>
     </div>
 </div>
 
